cabe459717d4efbdc071c02934d1ec126489da4e
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2023-12-01 13:12:48 +0000

    alpha v0.0.5

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,10 +1,8 @@
 import deco from "../assets/img/deco.png";
 import placeholder from "../assets/img/placeholder.jpg";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 const Layout = () => {
-  const [dati, setDati] = useState([]);
-
   const arrayRange = (start, stop, step) =>
     Array.from(
       { length: (stop - start) / step + 1 },
@@ -17,25 +15,28 @@ const Layout = () => {
   //   return { numero: el, isOpen: false };
   // });
 
-  const data = () => {
+  const initialData = arrDataDays.map((el) => {
+    return { numero: el, isOpen: false };
+  });
+
+  const [dati, setDati] = useState(initialData);
+
+  const strappaCasella = (id) => {
     setDati(
-      arrDataDays.map((el) => {
-        return { numero: el, isOpen: false };
+      dati.map((item) => {
+        if (item.numero == id.numero) {
+          return { ...item, isOpen: item.isOpen = true };
+        }
+        return item
       })
     );
   };
 
-  const strappaCasella = (id) => {
-    setDati(dati.map(item => item.numero == id.numero && {...item, isOpen: item.isOpen= true}));
-    console.log(dati)
-  };
-
-  useEffect(() => {
-    data();
-  }, []);
-
   const newDate = new Date();
-  const myDate = newDate.getDate();
+  //const myDate = newDate.getDate();
+  const myDate = 3;
+
+  console.log(dati);
 
   return (
     <main
@@ -52,26 +53,29 @@ const Layout = () => {
           <div
             key={el.numero}
             className={`relative bg-black/95 w-1/6 flex flex-col items-center cursor-pointer text-yellow-500 justify-center font-semibold
-            ${myDate > el.numero && "border-0 border-none"}
-            ${myDate == el.numero && "border-4 border-red-500 "}
+            ${myDate > el.numero && "border-0 border-none pointer-events-none"}
+            ${myDate == el.numero && "border-4 border-red-500"}
             ${
               myDate < el.numero &&
               "border border-t-0 border-dashed rounded-br-3xl pointer-events-none opacity-95"
             }
             `}
           >
-            {!el.isOpen && (
+            {(el.numero >= myDate) && (
               <strong
-                className="flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full text-center text-7xl z-50"
-                onClick={() => strappaCasella(el.numero)}
+                className={`flex items-center justify-center absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full text-center text-7xl z-50
+                ${el.numero == myDate && ""}
+                `}
+                onClick={() => strappaCasella(el)}
               >
                 {el.numero}
               </strong>
             )}
+            {(el.numero >= myDate || el.isOpen) && <div className="bg-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full z-40"></div>}
             <div
-              className="h-full w-full z-40 flex flex-col items-center justify-end"
+              className="h-full w-full z-20 flex flex-col items-center justify-end"
               style={
-                el.numero <= myDate && el.isOpen
+                el.numero <= myDate
                   ? {
                       backgroundImage: `url(${placeholder})`,
                       backgroundPosition: "center",
