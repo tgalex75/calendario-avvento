d76322b962e251d41fbfc662d6add3ee32c8d7be
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2024-11-26 08:47:31 +0000

    eliminate inutili cartelle

--- src/Data/db.jsx
+++
@@ -1,9 +0,0 @@
-import Dexie from "dexie";
-
-export const db = new Dexie("AdventCalendarDB");
-db.version(1).stores({
-  adventCalendar: "++id, day, isOpen",
-});
-
-const date = new Date()
-export const today = date.getUTCDate()
